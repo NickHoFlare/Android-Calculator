998919563a6c9f2652f68dce3e00018c07b312ec
Nicholas <terran324@gmail.com>
2017-01-18 13:10:29 +1100

    Finalized layout for calculator app

--- app/src/main/java/com/keez/nicho/calculator/MainActivity.java
+++ app/src/main/java/com/keez/nicho/calculator/MainActivity.java
@@ -2,12 +2,26 @@ package com.keez.nicho.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // "R" refers to resources
+
+        TextView textElement = (TextView)findViewById(R.id.calculator_instruction);
+        String instructionText = textElement.getText().toString();
+        //String modifiedText = instructionText + "This is rubbish ignore me";
+        //textElement.setText(modifiedText);
     }
+
+
 }
